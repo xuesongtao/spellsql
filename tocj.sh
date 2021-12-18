149d67f1c459526909140723f348cbba1fa51a67
xuesongtao <xuesongtao512@qq.com>
2021-12-18 13:49:38 +0800

    update

--- tocj.sh
+++ tocj.sh
@@ -13,26 +13,22 @@ function checkIsOk() {
     echo "[${1}] is success"
 }
 
-function updateSqlStr() {
+function updateCjGoFile() {
     # $1 目标文件
 
     # 更换包名 > 注释 log > 取消 glog 注释 > 注释 log. > 取消注释 glog.
-    echo "updateSqlStr curPath: ${curPath}"
     cd $curPath && checkIsOk "cd ${curPath}"
+
+    # 处理 getsqlstr.go
     sed -e "s/package spellsql/package mysql/g" \
         -e "s/\"log\"/\/\/ \"log\"/g" \
         -e "s/\/\/ \"github.com/\"github.com/g" \
         -e "s/log.P/\/\/ log.P/g" \
         -e "s/\/\/ glog.I/glog.I/g" \
-        spellsql.go >$1 && checkIsOk "updateTestSqlStr"
-}
+        spellsql.go >$1 && checkIsOk "update getsqlstr.go"
 
-function updateTestSqlStr() {
-    # $1 目标文件
-
-    # 更换包名
-    cd $curPath && checkIsOk "cd ${curPath}"
-    sed -e "s/package spellsql/package mysql/g" spellsql_test.go >$1 && checkIsOk "updateTestSqlStr"
+    # 处理 getsqlstr_test.go
+    sed -e "s/package spellsql/package mysql/g" spellsql_test.go >$1 && checkIsOk "update getsqlstr_test.go"
 }
 
 function gitHandle() {
@@ -46,16 +42,20 @@ function gitHandle() {
     git push && checkIsOk "git push"
 }
 
-function handle() {
+function startHandle() {
     # $1 gofile
 
     goFile=$1
     # 解析项目路径
     projectDirIndex=$(echo $goFile | awk 'BEGIN {print index("'$goFile'", "/app/model/mysql")}')
+    if [[ $projectDirIndex < 1 ]]; then
+        echo "parse projectDirIndex is failed"
+        return
+    fi
+
     projectDir=${goFile:0:$projectDirIndex-1}
     printf "======= 开始处理: %s ===========\n" $projectDir
-    updateSqlStr $goFile # 处理 getsqlstr.go
-    updateTestSqlStr $goFile # 处理 getsqlstr_test.go
+    updateCjGoFile $goFile
     gitHandle $projectDir
     printf "======= 处理成功: %s ===========\n" $projectDir
 }
@@ -64,16 +64,17 @@ function main() {
     # $1 gofile 有值只处理此项目中的内容 getsqlstr.go 和 getsqlstr_test.go
 
     if [[ $1 != "" ]]; then
-        handle $1
+        startHandle $1
         return
     fi
+
     for goFile in $(find "/Users/xuesongtao/goProject/src/workGo" -name "getsqlstr.go"); do
-        sleep 2s
-        handle $goFile
+        sleep 1s
+        startHandle $goFile
     done
 }
 
-main
+# main
 
-# main "/Users/xuesongtao/goProject/src/workGo/aesm/appside_server/app/model/mysql/getsqlstr.go"
+main "/Users/xuesongtao/goProject/src/workGo/aesm/appside_server/app/model/mysql/getsqlstr.go"
 # main "/Users/xuesongtao/goProject/src/workGo/aesm/appside_server/app/model/mysql/getsqlstr_test.go"
