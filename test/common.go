e566c321b49a1ce667bc7a9baaa4da48017bf988
xuesongtao <xuesongtao512@qq.com>
2023-04-15 22:26:59 +0800

    update

--- test/common.go
+++ test/common.go
@@ -14,6 +14,24 @@ const (
 	sureAge  = int32(18)
 )
 
+type Man struct {
+	Id       int32  `json:"id,omitempty" gorm:"id" db:"id"`
+	Name     string `json:"name,omitempty" gorm:"name" db:"name"`       // 姓名
+	SName    string `json:"s_name,omitempty" gorm:"s_name" db:"s_name"` // 学名
+	Age      int32  `json:"age,omitempty" gorm:"age" db:"age"`
+	Addr     string `json:"addr,omitempty" gorm:"addr" db:"addr"`
+	Hobby    string `json:"hobby,omitempty"`
+	NickName string `json:"nickname,omitempty" gorm:"nickname" db:"nickname"`
+	JsonTxt  Tmp    `json:"json_txt,omitempty"`
+	XmlTxt   Tmp    `json:"xml_txt,omitempty"`
+	Json1Txt Tmp    `json:"json1_txt,omitempty"`
+}
+
+type Tmp struct {
+	Name string `json:"name,omitempty" xml:"name"`
+	Data string `json:"data,omitempty" xml:"data"`
+}
+
 func StructValEqual(dest, src interface{}) bool {
 	destVal := reflect.ValueOf(dest)
 	srcVal := reflect.ValueOf(src)
@@ -37,22 +55,4 @@ func Equal(dest, src interface{}) bool {
 		fmt.Printf("src: %v\n", src)
 	}
 	return ok
-}
-
-type Man struct {
-	Id       int32  `json:"id,omitempty" gorm:"id" db:"id"`
-	Name     string `json:"name,omitempty" gorm:"name" db:"name"`       // 姓名
-	SName    string `json:"s_name,omitempty" gorm:"s_name" db:"s_name"` // 学名
-	Age      int32  `json:"age,omitempty" gorm:"age" db:"age"`
-	Addr     string `json:"addr,omitempty" gorm:"addr" db:"addr"`
-	Hobby    string `json:"hobby,omitempty"`
-	NickName string `json:"nickname,omitempty" gorm:"nickname" db:"nickname"`
-	JsonTxt  Tmp    `json:"json_txt,omitempty"`
-	XmlTxt   Tmp    `json:"xml_txt,omitempty"`
-	Json1Txt Tmp    `json:"json1_txt,omitempty"`
-}
-
-type Tmp struct {
-	Name string `json:"name,omitempty" xml:"name"`
-	Data string `json:"data,omitempty" xml:"data"`
 }
