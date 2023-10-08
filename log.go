46e85f9e10ff42c2ad4e25851c14259f8eab2e19
xuesongtao <xuesongtao512@qq.com>
2023-10-08 09:38:07 +0800

    添加: 支持 ctx

--- log.go
+++ log.go
@@ -2,10 +2,12 @@ package spellsql
 
 import (
 	"context"
-	"log"
 	"os"
 	"runtime"
 	"strconv"
+	
+	"log"
+	// slg "gitlab.cd.anpro/go/common/log"
 )
 
 type defaultLogger struct {
@@ -20,14 +22,29 @@ func NewLogger() *defaultLogger {
 
 func (d *defaultLogger) Info(ctx context.Context, v ...interface{}) {
 	d.log.Println(append([]interface{}{"[INFO] " + d.getPrefix(3)}, v...)...)
+	// slg.InfofWithTrace(ctx, d.getFormat(v...), v...)
 }
 
 func (d *defaultLogger) Error(ctx context.Context, v ...interface{}) {
 	d.log.Println(append([]interface{}{"[ERRO] " + d.getPrefix(3)}, v...)...)
+	// slg.ErrorfWithTrace(ctx, errors.New("sql handle err"), d.getFormat(v...), v...)
 }
 
 func (d *defaultLogger) Warning(ctx context.Context, v ...interface{}) {
 	d.log.Println(append([]interface{}{"[WARN] " + d.getPrefix(3)}, v...)...)
+	// slg.WarnfWithTrace(ctx, d.getFormat(v...), v...)
+}
+
+func (d *defaultLogger) getFormat(v ...interface{}) (formatStr string) {
+	l := len(v)
+	for i := 0; i < l; i++ {
+		if formatStr == "" {
+			formatStr = "%v"
+			continue
+		}
+		formatStr += " %v"
+	}
+	return
 }
 
 func (d *defaultLogger) getPrefix(skip int) string {
