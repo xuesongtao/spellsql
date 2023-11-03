fc2f7a4b2cf5095a6d2b151691f3fe0af91a1a5b
xuesongtao <xuesongtao512@qq.com>
2023-11-03 10:10:42 +0800

    添加: convert 对比

--- convert_test.go
+++ convert_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"reflect"
 	"testing"
+
+	"github.com/jinzhu/copier"
 )
 
 type TmpDest struct {
@@ -12,6 +14,7 @@ type TmpDest struct {
 	Hobby         []string   `json:"hobby,omitempty"`
 	NeedMarshal   string     `json:"need_marshal,omitempty"`
 	NeedUnmarshal []*TmpNest `json:"need_unmarshal,omitempty"`
+	Copy          []*TmpNest `json:"copy,omitempty"`
 }
 
 type TmpSrc struct {
@@ -21,6 +24,7 @@ type TmpSrc struct {
 	Test          string     `json:"test,omitempty"`
 	NeedMarshal   []*TmpNest `json:"need_marshal,omitempty"`
 	NeedUnmarshal string     `json:"need_unmarshal,omitempty"`
+	Copy          []*TmpNest `json:"copy,omitempty"`
 }
 
 type TmpNest struct {
@@ -94,13 +98,29 @@ func TestConvert(t *testing.T) {
 				NeedUnmarshal: []*TmpNest{{Name: "需要 unmarshal 测试"}},
 			},
 		},
+		{
+			desc: "copy-demo",
+			src: TmpSrc{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+				Copy:  []*TmpNest{{Name: "copy"}},
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+				Copy:  []*TmpNest{{Name: "copy"}},
+			},
+		},
 	}
+
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
 			obj := NewConvStruct()
 
 			obj.Init(&tC.src, &tC.dest)
-
 			if tC.desc == "需要marshal" {
 				obj.SrcMarshal(json.Marshal, "need_marshal")
 			}
@@ -112,10 +132,257 @@ func TestConvert(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			// tC.src.Name = "testname"
+
+			if tC.desc == "copy-demo" {
+				tC.src.Age = 0
+				tC.src.Copy[0].Name = "修改"
+				for _, v := range tC.dest.Copy {
+					t.Log(v)
+				}
+			}
+			t.Logf("dst: %+v", tC.dest)
 			if !reflect.DeepEqual(tC.dest, tC.ok) {
 				t.Errorf("convert is failed, dest: %+v, ok: %+v", tC.dest, tC.ok)
 			}
 		})
+	}
+}
+
+func TestCopyerConvert(t *testing.T) {
+	testCases := []struct {
+		desc string
+		src  TmpSrc
+		dest TmpDest
+		ok   TmpDest
+	}{
+		{
+			desc: "单字段",
+			src: TmpSrc{
+				Name: "name",
+				Age:  10,
+				Test: "test",
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name: "name",
+				Age:  10,
+			},
+		},
+		{
+			desc: "有切片字段",
+			src: TmpSrc{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+				Test:  "test",
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+			},
+		},
+		{
+			desc: "需要marshal",
+			src: TmpSrc{
+				Name:        "name",
+				Age:         10,
+				Hobby:       []string{"打篮球", "跑步"},
+				NeedMarshal: []*TmpNest{{Name: "需要 marshal 测试"}},
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:        "name",
+				Age:         10,
+				Hobby:       []string{"打篮球", "跑步"},
+				NeedMarshal: "[{\"name\":\"需要 marshal 测试\"}]",
+			},
+		},
+		{
+			desc: "需要unmarshal",
+			src: TmpSrc{
+				Name:          "name",
+				Age:           10,
+				Hobby:         []string{"打篮球", "跑步"},
+				NeedUnmarshal: "[{\"name\":\"需要 unmarshal 测试\"}]",
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:          "name",
+				Age:           10,
+				Hobby:         []string{"打篮球", "跑步"},
+				NeedUnmarshal: []*TmpNest{{Name: "需要 unmarshal 测试"}},
+			},
+		},
+		{
+			desc: "copy-demo",
+			src: TmpSrc{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+				Copy:  []*TmpNest{{Name: "copy"}},
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+				Copy:  []*TmpNest{{Name: "copy"}},
+			},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			err := copier.Copy(&tC.dest, &tC.src)
+			if err != nil {
+				t.Error("err:", err)
+			}
+			t.Logf("dst: %+v", tC.dest)
+			if !reflect.DeepEqual(tC.dest, tC.ok) {
+				t.Errorf("convert is failed, dest: %+v, ok: %+v", tC.dest, tC.ok)
+			}
+		})
+	}
+}
+
+func TestJsonConvert(t *testing.T) {
+	testCases := []struct {
+		desc string
+		src  TmpSrc
+		dest TmpDest
+		ok   TmpDest
+	}{
+		{
+			desc: "单字段",
+			src: TmpSrc{
+				Name: "name",
+				Age:  10,
+				Test: "test",
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name: "name",
+				Age:  10,
+			},
+		},
+		{
+			desc: "有切片字段",
+			src: TmpSrc{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+				Test:  "test",
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+			},
+		},
+		{
+			desc: "需要marshal",
+			src: TmpSrc{
+				Name:        "name",
+				Age:         10,
+				Hobby:       []string{"打篮球", "跑步"},
+				NeedMarshal: []*TmpNest{{Name: "需要 marshal 测试"}},
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:        "name",
+				Age:         10,
+				Hobby:       []string{"打篮球", "跑步"},
+				NeedMarshal: "[{\"name\":\"需要 marshal 测试\"}]",
+			},
+		},
+		{
+			desc: "需要unmarshal",
+			src: TmpSrc{
+				Name:          "name",
+				Age:           10,
+				Hobby:         []string{"打篮球", "跑步"},
+				NeedUnmarshal: "[{\"name\":\"需要 unmarshal 测试\"}]",
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:          "name",
+				Age:           10,
+				Hobby:         []string{"打篮球", "跑步"},
+				NeedUnmarshal: []*TmpNest{{Name: "需要 unmarshal 测试"}},
+			},
+		},
+		{
+			desc: "copy-demo",
+			src: TmpSrc{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+				Copy:  []*TmpNest{{Name: "copy"}},
+			},
+			dest: TmpDest{},
+			ok: TmpDest{
+				Name:  "name",
+				Age:   10,
+				Hobby: []string{"打篮球", "跑步"},
+				Copy:  []*TmpNest{{Name: "copy"}},
+			},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			b, _ := json.Marshal(&tC.src)
+			if err := json.Unmarshal(b, &tC.dest); err != nil {
+				t.Error("json unmarshal err:", err)
+			}
+			t.Logf("dst: %+v", tC.dest)
+			if !reflect.DeepEqual(tC.dest, tC.ok) {
+				t.Errorf("convert is failed, dest: %+v, ok: %+v", tC.dest, tC.ok)
+			}
+		})
+	}
+}
+
+func BenchmarkConvert(b *testing.B) {
+	src := TmpSrc{
+		Name:        "name",
+		Age:         10,
+		Hobby:       []string{"打篮球", "跑步"},
+		NeedMarshal: []*TmpNest{{Name: "需要 marshal 测试"}},
+	}
+	dest := TmpDest{}
+	for i := 0; i < b.N; i++ {
+		obj := NewConvStruct()
+		obj.Init(&src, &dest)
+		_ = obj.Convert()
+	}
+}
+
+func BenchmarkJsonConvert(b *testing.B) {
+	src := TmpSrc{
+		Name:        "name",
+		Age:         10,
+		Hobby:       []string{"打篮球", "跑步"},
+		NeedMarshal: []*TmpNest{{Name: "需要 marshal 测试"}},
+	}
+	dest := TmpDest{}
+	for i := 0; i < b.N; i++ {
+		b, _ := json.Marshal(&src)
+		json.Unmarshal(b, &dest)
+	}
+}
+func BenchmarkCopyerConvert(b *testing.B) {
+	src := TmpSrc{
+		Name:        "name",
+		Age:         10,
+		Hobby:       []string{"打篮球", "跑步"},
+		NeedMarshal: []*TmpNest{{Name: "需要 marshal 测试"}},
+	}
+	dest := TmpDest{}
+	for i := 0; i < b.N; i++ {
+		copier.Copy(&dest, &src)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jinzhu/copier v0.4.0
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.2.0
 	gorm.io/driver/mysql v1.3.2
